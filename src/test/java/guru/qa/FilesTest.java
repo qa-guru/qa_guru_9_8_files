e75801412bf21089df3ac9cfec27d722b9cf85f1
dtuchs <d.tuchs@propellerads.net>
2021-11-11 20:35:11 +0300

    java files examples

---
+++ src/test/java/guru/qa/FilesTest.java
@@ -0,0 +1,118 @@
+package guru.qa;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.selector.ByText;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FilesTest {
+
+    @Test
+    @DisplayName("Загрузка файла по абсолютному пути (не рекомендуется)")
+    void filenameShouldDisplayedAfterUploadActionAbsolutePathTest() {
+        open("https://the-internet.herokuapp.com/upload");
+        File exampleFile = new File("/Users/dmitrytuchs/IdeaProjects/qaguru/qa_guru_9_8_files/src/test/resources/example.txt");
+        $("input[type='file']").uploadFile(exampleFile);
+        $("#file-submit").click();
+        $("#uploaded-files").shouldHave(text("example.txt"));
+    }
+
+    @Test
+    @DisplayName("Загрузка файла по относительному пути (рекомендуется!)")
+    void filenameShouldDisplayedAfterUploadActionFromClasspathTest() {
+        open("https://the-internet.herokuapp.com/upload");
+        $("input[type='file']").uploadFromClasspath("example.txt");
+        $("#file-submit").click();
+        $("#uploaded-files").shouldHave(text("example.txt"));
+    }
+
+    @Test
+    @DisplayName("Скачивание текстового файла и проверка его содержимого")
+    void downloadSimpleTextFileTest() throws IOException {
+        open("https://github.com/junit-team/junit5/blob/main/README.md");
+        File download = $("#raw-url").download();
+        String fileContent = IOUtils.toString(new FileReader(download));
+        assertTrue(fileContent.contains("This repository is the home of the next generation of JUnit, _JUnit 5_."));
+    }
+
+    @Test
+    @DisplayName("Скачивание PDF файла")
+    void pdfFileDownloadTest() throws IOException {
+        open("https://junit.org/junit5/docs/current/user-guide/");
+        File pdf = $(byText("PDF download")).download();
+        PDF parsedPdf = new PDF(pdf);
+        Assertions.assertEquals(164, parsedPdf.numberOfPages);
+    }
+
+    @Test
+    @DisplayName("Скачивание XLS файла")
+    void xlsFileDownloadTest() throws IOException {
+        open("http://romashka2008.ru/price");
+        File file = $$("a[href*='prajs']")
+                .find(text("Скачать Прайс-лист Excel"))
+                .download();
+
+        XLS parsedXls = new XLS(file);
+        boolean checkPassed = parsedXls.excel
+                .getSheetAt(0)
+                .getRow(11)
+                .getCell(1)
+                .getStringCellValue()
+                .contains("693010, Сахалинская обл, Южно-Сахалинск г, им Анкудинова Федора Степановича б-р, дом № 15, корпус А");
+
+        assertTrue(checkPassed);
+    }
+
+    @Test
+    @DisplayName("Парсинг CSV файлов")
+    void parseCsvFileTest() throws IOException, CsvException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream("csv.csv");
+             Reader reader = new InputStreamReader(is)) {
+            CSVReader csvReader = new CSVReader(reader);
+
+            List<String[]> strings = csvReader.readAll();
+            assertEquals(3, strings.size());
+        }
+    }
+
+    @Test
+    @DisplayName("Парсинг ZIP файлов")
+    void parseZipFileTest() throws IOException, CsvException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream("zip_2MB.zip");
+            ZipInputStream zis = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                System.out.println(entry.getName());
+            }
+        }
+    }
+
+}
